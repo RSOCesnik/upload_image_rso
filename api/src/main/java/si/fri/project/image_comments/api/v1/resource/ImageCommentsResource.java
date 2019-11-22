877c88be580f85eae86b5a3acaa409e495b0b052
cesnik <cesnik@localhost.localdomain>
2019-11-22 21:58:11 +0100

    change port and path

--- api/src/main/java/si/fri/project/image_comments/api/v1/resource/ImageCommentsResource.java
+++ api/src/main/java/si/fri/project/image_comments/api/v1/resource/ImageCommentsResource.java
@@ -8,43 +8,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @ApplicationScoped
-@Path("/comments")
+@Path("/uploads")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ImageCommentsResource {
 
     @GET
     public Response getComments() {
-        Map<String, Object> map = new HashMap<String, Object>();
-        List<String> clani = new ArrayList<String>();
-        clani.add("bc1610");
-
-        List<String> mikrostoritve = new ArrayList<String>();
-        mikrostoritve.add("");
-
-        List<String> github = new ArrayList<String>();
-        github.add("https://github.com/RSOCesnik/upload_image_rso");
-
-
-        List<String> travis = new ArrayList<String>();
-        travis.add("https://travis-ci.org/RSOCesnik/upload_image_rso");
-
-        List<String> dockerhub = new ArrayList<String>();
-        dockerhub.add("bc1610");
-
-
-        map.put("clani", clani);
-        map.put("opis_projekta", "S pomocjo nasega projekta, lahko dodajate slike, jih gledate ali komentirate");
-        map.put("mikrostoritve", mikrostoritve);
-        map.put("github", github);
-        map.put("travis", travis);
-        map.put("dockerhub", dockerhub);
-        return Response.ok(map).build();
+        return Response.ok("UPLOADER API").build();
     }
 }
